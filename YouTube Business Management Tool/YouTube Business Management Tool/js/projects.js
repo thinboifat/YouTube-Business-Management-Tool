b8f44bd02a45c4d307cb903cc985f1d44e0f1ba5
ITX Desktop <up647395@myport.ac.uk>
2017-03-21 15:07:52 +0000

    Major overhaul to page layout. One page design mechanics implemented

--- YouTube Business Management Tool/YouTube Business Management Tool/js/projects.js
+++ YouTube Business Management Tool/YouTube Business Management Tool/js/projects.js
@@ -1,45 +1,46 @@
 ﻿
 
+function setUpSubmit() {
+    $(document).ready(function () {
+        $("#submit").click(function () {
 
-$(document).ready(function () {
-    $("#submit").click(function () {
+            var projStatus = $("#projStatus").val();
+            var details = $("#details").val();
+            var actType = "Update";
 
-        var projStatus = $("#projStatus").val();
-        var details = $("#details").val();
-        var actType = "Update";
+            var newVideoURL = $("#videoURL").val();
 
-        var newVideoURL = $("#videoURL").val();
-        
 
-        if (projStatus === 'Production' || projStatus === 'Filmed' || projStatus === 'Edited') { actType = "Production" };
-        if (projStatus === 'Published') { actType = "Publish" };
-        if (projStatus === 'Complete') { actType = "Completion" };
+            if (projStatus === 'Production' || projStatus === 'Filmed' || projStatus === 'Edited') { actType = "Production" };
+            if (projStatus === 'Published') { actType = "Publish" };
+            if (projStatus === 'Complete') { actType = "Completion" };
 
-        
-        var projName = $("#videoList").val();
-        //Insert query
 
-        // Returns successful data submission message when the entered information is stored in database.
-        var dataString = 'projName=' + projName + '&details=' + details + '&actType=' + actType + '&projStatus=' + projStatus + '&videoURL=' + newVideoURL;
+            var projName = $("#videoList").val();
+            //Insert query
 
-        if (details == '') {
-            alert("Please add details");
-        }
-        else {
-            // AJAX Code To Submit Form.
-            $.ajax({
-                type: "POST",
-                url: "../includes/activitySubmit.php",
-                data: dataString,
-                cache: false,
-                success: function (result) {
-                    updateVideoElements();
-                }
-            });
-        }
-        return false;
+            // Returns successful data submission message when the entered information is stored in database.
+            var dataString = 'projName=' + projName + '&details=' + details + '&actType=' + actType + '&projStatus=' + projStatus + '&videoURL=' + newVideoURL;
+
+            if (details == '') {
+                alert("Please add details");
+            }
+            else {
+                // AJAX Code To Submit Form.
+                $.ajax({
+                    type: "POST",
+                    url: "../includes/activitySubmit.php",
+                    data: dataString,
+                    cache: false,
+                    success: function (result) {
+                        updateVideoElements();
+                    }
+                });
+            }
+            return false;
+        });
     });
-});
+};
 
 //Update the video elements on initial page load
 $(document).ready(function() {
@@ -54,16 +55,68 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $('#contentSwitch').change(function () {
+        var isProjectManager = $(this).prop('checked');
+        
+        if (isProjectManager === true) {
+            updateVideoElements("true")
+        }
+        else {
+            updateVideoElements("false")
+        }
+
+        
+    })
+})
+
 //Update all the div elements that use video project data
-function updateVideoElements() {
-    var videoList = $("#videoList").val();
-    var projectStatus = "test";
+function updateVideoElements(isProjectManager) {
 
-    $("#projectName").html(videoList);
-    
-    getSelectionStatus(videoList);
-    getSelectionTimeline(videoList);
+    if (isProjectManager === "false") {
+        $("#projectName").html("New Project");
+        getProjectCreationHelp();
+        getNewProjectContent();
+        getCalendarContent();
+        //$("#mainSectionHeader").html("New Project");
 
+    }
+    else {
+        // Load selection choice
+        // Code to do
+        //$("#mainSectionHeader").html('');
+
+        var videoList = $("#videoList").val();
+        //var projectStatus = "test";
+
+        $("#projectName").html(videoList);
+        getSelectionStatus(videoList);
+        getSelectionTimeline(videoList);
+        getActivityBar();
+        setUpSubmit();
+    }
+
+}
+
+function getProjectCreationHelp() {
+    $("#projectStatus").html("Video help tool here. <br/> Popular types.<br/> Maybe google trends?");
+}
+
+function getNewProjectContent() {
+    $("#mainBody").html("New Project Here");
+}
+
+// Update the Second Side Bar
+function getCalendarContent() {
+    $("#newActivity").html("Calendar View Here!");
+}
+
+// Update the Second Side Bar
+function getActivityBar() {
+    var actContent = '<form><div class="form-group"><label>New Status</label><select class="form-control" id="projStatus"><option></option><option>Planning</option><option>Awaiting Item</option><option>Pre-Production</option><option>Production</option><option>Filmed</option><option>Edited</option><option>Published</option><option>Item To Return</option><option>Complete</option></select></div><div id="additionalForms"></div><div class="form-group"><label for="details">Details</label><textarea class="form-control" id="details" rows="4"></textarea></div><button type="button" id="submit" class="btn btn-primary">Submit</button></form>';
+
+    $("#newActivity").html(actContent);
+    activityLoader();
 }
 
 function getSelectionStatus(input) {
@@ -135,7 +188,7 @@ function getSelectionStatus(input) {
 
         },
         error: function () {
-            alert("Fail")
+            alert("Failure loading timetime")
         }
 
     });
@@ -167,18 +220,20 @@ function getSelectionStatus(input) {
 
     }
 
-    // When the user makes an activity selection, update form with additional options
-    $(document).ready(function () {
-        $("#projStatus").change(function () {
-            var value = "";
-            value = $("#projStatus option:selected").text();
+// When the user makes an activity selection, update form with additional options
+    function activityLoader() {
+        $(document).ready(function () {
+            $("#projStatus").change(function () {
+                var value = "";
+                value = $("#projStatus option:selected").text();
 
-            if (value === "Published") {
-                $("#additionalForms").html($('<div class="form-group"><label for="videoURL">YouTube URL</label> <textarea class="form-control" id="videoURL" placeholder="The YouTube Video Code" rows="1"></textarea> </div>'));
-            }
-            else if (value === "Complete") {
-                $("#additionalForms").html('<div class="form-group"> <label>Item Status</label> <select class="form-control" id="itemStatus"> <option>Returned</option> <option>Archived</option></select></div>')
-            }
-            else { $("#additionalForms").html($('')); }
+                if (value === "Published") {
+                    $("#additionalForms").html($('<div class="form-group"><label for="videoURL">YouTube URL</label> <textarea class="form-control" id="videoURL" placeholder="The YouTube Video Code" rows="1"></textarea> </div>'));
+                }
+                else if (value === "Complete") {
+                    $("#additionalForms").html('<div class="form-group"> <label>Item Status</label> <select class="form-control" id="itemStatus"> <option>Returned</option> <option>Archived</option></select></div>')
+                }
+                else { $("#additionalForms").html($('')); }
+            });
         });
-    });
+    };
--- YouTube Business Management Tool/YouTube Business Management Tool/pages/projects.php
+++ YouTube Business Management Tool/YouTube Business Management Tool/pages/projects.php
@@ -25,6 +25,8 @@ include_once "../includes/projectsDashboard.php";
     <!-- Bootstrap Core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+
+
     <!-- MetisMenu CSS -->
     <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
@@ -40,6 +42,10 @@ include_once "../includes/projectsDashboard.php";
 
     <!-- jQuery -->
     <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Toggle Core CSS -->
+    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 
     <!--<script src="../js/typeahead.min.js"></script>-->
     
@@ -360,14 +366,22 @@ include_once "../includes/projectsDashboard.php";
                 </div>
 
             <div class="col-lg-12">
-                <h2 id="projectName"></h2>
-                <div id="test"></div>
-            </div>
+                <div class="row">
+                    <div class="col-lg-8">
+                        <h2 id="projectName"></h2>
+                    </div>
+                        <div class="col-lg-4">
+                            <input id="contentSwitch" type="checkbox" checked data-toggle="toggle" data-on="New Project" data-off="Project Manager" data-onstyle="info" data-offstyle="success">
+                        </div>
+                    
+                    <div id="test"></div>
+                </div>
+                </div>
 
             <div class="col-lg-9">
                 
                 <div class="panel panel-default">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="mainSectionHeader">
                         
                             <?php getProjectsSelect() ?>
                         
@@ -404,29 +418,7 @@ include_once "../includes/projectsDashboard.php";
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body newActivity" id="newActivity">
-                            <form>
-                                <div class="form-group">
-                                    <label>New Status</label>
-                                    <select class="form-control" id="projStatus">
-                                        <option></option>
-                                        <option>Planning</option>
-                                        <option>Awaiting Item</option>
-                                        <option>Pre-Production</option>
-                                        <option>Production</option>
-                                        <option>Filmed</option>
-                                        <option>Edited</option>
-                                        <option>Published</option>
-                                        <option>Item To Return</option>
-                                        <option>Complete</option>
-                                    </select>
-                                </div>
-                                <div id="additionalForms"></div>
-                                <div class="form-group">
-                                    <label for="details">Details</label>
-                                    <textarea class="form-control" id="details" rows="4"></textarea>
-                                </div>
-                                <button type="button" id="submit" class="btn btn-primary">Submit</button>
-                            </form>
+                            
                         </div>
                     </div>
                 </div>
