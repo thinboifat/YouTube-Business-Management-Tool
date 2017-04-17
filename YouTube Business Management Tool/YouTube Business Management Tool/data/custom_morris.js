bf8881bbd1530f1906a28090bbf8b5df1b9cbb00
ITX Desktop <up647395@myport.ac.uk>
2017-04-17 20:23:48 +0100

    Added Bar Item Graph

--- YouTube Business Management Tool/YouTube Business Management Tool/data/custom_morris.js
+++ YouTube Business Management Tool/YouTube Business Management Tool/data/custom_morris.js
@@ -4,7 +4,7 @@ $(function () {
     var graph = Morris.Donut({
         element: 'morris-donut-chart',
         data: [{ label: 'No Items!', value: 1 }],
-        colors: ["#AF2222", "#22AF22", "#2222AF", "#7F7000", "#707F00", "#70007F"],
+        colors: ["#AF2222", "#22AF22", "#2222AF", "#7F70c0", "#707F70", "#70007F"],
         resize: true
     });
 
@@ -25,43 +25,31 @@ $(function () {
 
     });
 
+    $.ajax({
+        url: '../includes/itemsBarChart.php',
+        'dataType': "json",
+        success: function (response)          //on recieve of reply
+        {
+            Morris.Bar({
+                element: 'morris-bar-chart',
+                data: response,
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Items In', 'Items Out'],
+                hideHover: 'auto',
+                resize: true
+            });
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log(
+              'status:' + XMLHttpRequest.status +
+              ', status text: ' + XMLHttpRequest.statusText
+            );
+            console.log(errorThrown);
+        }
 
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        hideHover: 'auto',
-        resize: true
     });
+
+
     
 });
---
+++ YouTube Business Management Tool/YouTube Business Management Tool/includes/itemsBarChart.php
@@ -0,0 +1,29 @@
+<?php
+
+include "../includes/databaseConn.php";
+
+$sql = "exec SP_Items_GetMonthlyGraph";
+
+
+$stmt = sqlsrv_query( $conn, $sql );
+if( $stmt === false) {
+    die( print_r( sqlsrv_errors(), true) );
+}
+
+$array = array();
+
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+        array_push(
+            $array,
+            array(
+                 'y' => $row['Month'],
+                 'a' => $row['Items_In'],
+                 'b' => $row['Items_Out']
+            )
+        );
+    }
+    echo json_encode($array);
+
+sqlsrv_free_stmt( $stmt);
+
+?>
